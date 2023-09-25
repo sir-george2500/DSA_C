b88ab206ba0b9fdb33598f92e0686c95592a5ff7
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-25 18:58:05 +0000

    added printList

--- DataStructure/link_list/simple_link_list/link_list.c
+++ DataStructure/link_list/simple_link_list/link_list.c
@@ -6,3 +6,39 @@ typedef struct node
 	int val;
 	struct node *next;
 }Node;
+
+void printList(Node *head)
+{
+	if(head == NULL)
+	{
+		return;
+	}
+
+	while(head != NULL)
+	{
+		printf("%d => ", head->val);
+		head = head->next;
+	}
+
+	printf("%s\n","NULL");
+}
+
+int main(void)
+{
+	Node *head = malloc(sizeof(head));
+
+	head->val =2;
+
+	Node *second = malloc(sizeof(second));
+
+	head->next = second;
+
+	second->val = 4;
+
+	second->val = NULL;
+
+	free(head);
+	free(second);
+
+	return (0);
+}
