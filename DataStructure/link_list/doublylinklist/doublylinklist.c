2d9423f9c01b0e60307312afa5ad8ba973be489c
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-30 02:11:05 +0000

    created the function to add Element to a doublylinklist

--- DataStructure/link_list/doublylinklist/doublylinklist.c
+++ DataStructure/link_list/doublylinklist/doublylinklist.c
@@ -16,14 +16,46 @@ void printList(Node *head){
 	 return;
 	}
 
-	while(head){
+	while(head!=NULL){
 	  printf("%d <=> ", head->val);
+	  head = head->next;
 	}
 	printf("%s\n", "NULL");
 }
 
+void addElement(Node **head, int data)
+{
+	Node *new_node = malloc(sizeof(Node));
+
+	new_node->val = data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+
+	if(*head == 0)
+	{
+	    *head = new_node;
+	    return; 
+	}
+
+        Node *current = *head;
+
+	while(current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	current->next = new_node;
+	new_node = current;
+}	
 
 int main(void)
 {
+	Node *head = NULL;
+
+	addElement(&head, 20);
+	addElement(&head, 40);
+
+	printList(head);
 	return (0);
 }
